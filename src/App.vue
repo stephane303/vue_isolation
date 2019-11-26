46040626726d5a7773b5f9ce6ee1a59cdc4af918
Stephane <gerber.stephane@gmail.com>
2019-11-26 06:57:28 +0100

    init

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld  msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
@@ -16,7 +16,7 @@ export default {
 }
 </script>
 
-<style>
+<style >
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
+import * as shadow from 'vue-shadow-dom'
 
 Vue.config.productionTip = false
+Vue.use(BootstrapVue);
+
+Vue.use(shadow)
 
 new Vue({
   render: h => h(App),
